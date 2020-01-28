9598066aaa6d43be2ce646d416b3c0498dccf598
wesdizzle1 <60222515+wesdizzle1@users.noreply.github.com>
2020-01-27 21:09:21 -0500

    Update 2.3 Project.py

--- 2.3 Project.py
+++ 2.3 Project.py
@@ -1,5 +1,21 @@
 first_name = input()
 last_name = input()
 
-print('Hello my name is',first_name, last_name)
-      
+print('Hello, friend what is your name?')
+
+print('Nice to meet you',first_name, last_name)
+
+favorite_place = input()
+
+print('What is your favorite place to eat?\n',favorite_place)
+
+if favorite_place == Chipotle:
+   print('Wow, that is mine too!')
+elif favorite_place == Chickfila:
+   print('Not a bad choice.')
+elif favorite_place == Dominos:
+   print('I love pizza!.')
+elif favorite_place == Panera:
+   print('I do not enjoy their food.')
+else:
+   print('Maybe I should check it out!')
